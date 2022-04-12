3c95edff3049e038aeb87e80474bbff940154cea
Keith Allatt <keith.allatt@gmail.com>
2022-04-12 12:37:42 -0400

    Removed alerts in md_to_html.js.

--- md_to_html.js
+++ md_to_html.js
@@ -1,22 +1,24 @@
 function addReadMe(document_id) {
     var converter = new showdown.Converter();
     let doc_element = document.getElementById(document_id);
+    if (doc_element === null) {
+        return false;
+    }
 
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://raw.githubusercontent.com/keithallatt/"+document_id+"/master/README.md");
     xhr.send();
 
     xhr.onload = function() {
-      if (xhr.status == 200) {
-          doc_element.innerHTML = converter.makeHtml(`${xhr.response}`);
+        if (xhr.status == 200) {
+            doc_element.innerHTML = converter.makeHtml(`${xhr.response}`);
 
-          // replace `<img src="` with `<img src="https://github.com/kallatt/img file` type
-          // thing in html to make images work.
-
-          alert(doc_element.innerHTML);
-      } else {
-          doc_element.innerHTML = `Failed to load from ${readme_url}: Status code ${xhr.status}`;
-      }
+            // replace `<img src="` with `<img src="https://github.com/kallatt/img file` type
+            // thing in html to make images work.
+        } else {
+            doc_element.innerHTML = `Failed to load from ${readme_url}: Status code ${xhr.status}`;
+        }
+        return true;
     };
 
     xhr.onerror = function() { // only triggers if the request couldn't be made at all
@@ -24,5 +26,5 @@ function addReadMe(document_id) {
     };
 }
 
-const repo_array = ["auto-update-project", "CustomIDE", "logisim-cpu", "Logic", "GameTools"];
+const repo_array = ["auto-updtttate-project", "CustomIDE", "logisim-cpu", "Logic", "GameTools"];
 repo_array.forEach(element => addReadMe(element));
